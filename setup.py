8cb2ea6ac86e1b2201ecbee25d62b8bc40dfc353
Ali Bozorgkhan <alibozorgkhan@gmail.com>
2015-03-17 20:47:36 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+print required
+
+setup(name='Hidemyass Scraper',
+      version='1.0',
+      description='Scrapes proxies on hidemyass.com',
+      author='Ali Bozorgkhan',
+      author_email='alibozorgkhan@gmail.com',
+      url='https://github.com/alibozorgkhan/hidemyass-scraper',
+      install_requires=required,
+      license='The MIT License (MIT)'
+     )
