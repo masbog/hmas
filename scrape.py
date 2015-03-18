514cb0c9fd4be0fc6cf366cd52a4c83a3e7939f6
Ali Bozorgkhan <alibozorgkhan@gmail.com>
2015-03-17 20:07:23 -0700

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,37 @@
+import time
+
+from selenium import webdriver
+
+
+browser = webdriver.PhantomJS()
+browser.get('http://proxylist.hidemyass.com/')
+
+while True:
+    table = browser.find_element_by_tag_name('table')
+    tbody = table.find_element_by_tag_name('tbody')
+    trs = tbody.find_elements_by_tag_name('tr')
+    for tr in trs:
+        tds = tr.find_elements_by_tag_name('td')
+        last_update = tds[0].text
+        ip = tds[1].text
+        port = int(tds[2].text)
+        country = tds[3].text
+        speed_indicator = tds[4].find_element_by_class_name('indicator')
+        speed_indicator_style = speed_indicator.get_attribute('style')
+        speed = int(speed_indicator_style.split('width:')[1].split('%')[0])
+        connection_time_indicator = tds[5].find_element_by_class_name('indicator')
+        connection_time_indicator_style = connection_time_indicator.get_attribute('style')
+        connection_time = int(connection_time_indicator_style.split('width:')[1].split('%')[0])
+        typ = tds[6].text
+        anonymity = tds[7].text
+
+        print [last_update, ip, port, country, speed, connection_time, typ, anonymity]
+
+    next_button = browser.find_element_by_class_name('pagination')\
+                         .find_element_by_xpath('//a[@class="next"]')
+    if not next_button:
+        break
+
+    next_button.click()
+
+    time.sleep(5)
